7d42caa0304387fef9bc2fa22e856fc5138c5af5
Georgiy <venom369@yandex.ru>
2017-03-25 01:31:30 +0300

    Core changes of algorithm.

--- src/ListOfGroupedElements.java
+++ src/ListOfGroupedElements.java
@@ -57,36 +57,44 @@ public class ListOfGroupedElements {
     }
 
     public void Merge(TripleLong tripleLong){
-        ArrayList<Integer> counter = new ArrayList<>();
-        int offset = 0;
+        ArrayList<Integer> mergingIndexList = new ArrayList<>();
+        boolean listsAlreadyMerged=false;
+        int removedGroups = 0;
         for (int i = 0; i < groups.size(); i++) {
-            if (!groups.get(i - offset).IsUnique(tripleLong)) {
-                counter.add(i - offset);
-                groups.get(counter.get(0)).Add(tripleLong);
-                if (counter.size() == 2) {
-                    System.out.println("Merging groups");
-                    groups.get(counter.get(0)).Add(groups.get(counter.get(1)));
-                    counter.remove(1);
-                    groups.remove(i - offset);
-                    offset++;
+            if (!groups.get(i - removedGroups).IsUnique(tripleLong)) {
+                mergingIndexList.add(i - removedGroups);
+                if (!listsAlreadyMerged){
+                    groups.get(mergingIndexList.get(0)).Add(tripleLong);
+                    listsAlreadyMerged=true;
+                }
+                if (mergingIndexList.size() == 2) {
+                    System.out.println("Merging triplelong");
+                    groups.get(mergingIndexList.get(0)).Add(groups.get(mergingIndexList.get(1)));
+                    mergingIndexList.remove(1);
+                    groups.remove(i - removedGroups);
+                    removedGroups++;
                 }
             }
         }
     }
 
     public void Merge(Group group){
-        ArrayList<Integer> counter = new ArrayList<>();
-        int offset = 0;
+        ArrayList<Integer> mergingIndexList = new ArrayList<>();
+        boolean listsAlreadyMerged=false;
+        int removedGroups = 0;
         for (int i = 0; i < groups.size(); i++) {
-            if (groups.get(i - offset).IsIntersect(group)) {
-                counter.add(i - offset);
-                groups.get(counter.get(0)).Add(group);
-                if (counter.size() == 2) {
+            if (groups.get(i - removedGroups).IsIntersect(group)) {
+                mergingIndexList.add(i - removedGroups);
+                if (!listsAlreadyMerged){
+                    groups.get(mergingIndexList.get(0)).Add(group);
+                    listsAlreadyMerged=true;
+                }
+                if (mergingIndexList.size() == 2) {
                     System.out.println("Merging groups");
-                    groups.get(counter.get(0)).Add(groups.get(counter.get(1)));
-                    counter.remove(1);
-                    groups.remove(i - offset);
-                    offset++;
+                    groups.get(mergingIndexList.get(0)).Add(groups.get(mergingIndexList.get(1)));
+                    mergingIndexList.remove(1);
+                    groups.remove(i - removedGroups);
+                    removedGroups++;
                 }
             }
         }
